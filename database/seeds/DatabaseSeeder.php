877fc809abac7d6178dc3624dbdb3ea30c63952b
dudyali <dudyali@gmail.com>
2018-09-26 06:46:27 +0700

    updates

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Eloquent::unguard();
-        // $path = storage_path('master/kecamatan.sql');
-        // DB::unprepared(file_get_contents($path));
-        // $this->command->info('Table Kecamatan seeded!');
+        Eloquent::unguard();
+        $path = storage_path('master/kecamatan.sql');
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Table Kecamatan seeded!');
 
         // $this->call(SubyekTableSeeder::class);
-        $this->call(KategoriSeederTable::class);
+        // $this->call(KategoriSeederTable::class);
     }
 }
