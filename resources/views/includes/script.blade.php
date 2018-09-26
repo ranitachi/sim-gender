7770bddf89d8a602073cecb385db690d7eb35240
ranitachi <fachran.nazarullah@gmail.com>
2018-09-26 14:09:56 +0700

    update

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,6 +1,7 @@
 <!-- Global stylesheets -->
 <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
 <link href="{{asset('assets/css/icons/icomoon/styles.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('assets/css/icons/fontawesome/styles.min.css')}}" rel="stylesheet" type="text/css">
 <link href="{{asset('assets/css/bootstrap.css')}}" rel="stylesheet" type="text/css">
 <link href="{{asset('assets/css/core.css')}}" rel="stylesheet" type="text/css">
 <link href="{{asset('assets/css/components.css')}}" rel="stylesheet" type="text/css">
@@ -15,15 +16,20 @@
 <!-- /core JS files -->
 
 <!-- Theme JS files -->
-<script type="text/javascript" src="{{asset('assets/js/plugins/visualization/d3/d3.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/plugins/visualization/d3/d3_tooltip.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/plugins/forms/styling/switchery.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/plugins/forms/styling/uniform.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/plugins/forms/selects/bootstrap_multiselect.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/plugins/ui/moment/moment.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/plugins/pickers/daterangepicker.js')}}"></script>
 
+<script type="text/javascript" src="{{asset('assets/js/plugins/visualization/echarts/echarts.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/js/plugins/tables/datatables/datatables.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/js/plugins/forms/selects/select2.min.js')}}"></script>
+
 <script type="text/javascript" src="{{asset('assets/js/core/app.js')}}"></script>
 {{-- <script type="text/javascript" src="{{asset('assets/js/pages/dashboard.js')}}"></script> --}}
 <script type="text/javascript" src="{{asset('assets/js/pages/layout_fixed_native.js')}}"></script>
-<!-- /theme JS files -->
+
+{{-- <script type="text/javascript" src="{{asset('assets/js/pages/datatables_basic.js')}}"></script> --}}
+
+
