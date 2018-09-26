a6aee6a935c5dbe8c8e056f785177467ff9e9ef9
dudyali <dudyali@gmail.com>
2018-09-26 14:10:59 +0700

    kepadatan controller

---
+++ app/Http/Controllers/KependudukanKepadatanController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class KependudukanKepadatanController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+        
+    }
+}
